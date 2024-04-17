6f03639da921a6ec812322a00e543e29d917cf4b
ShirsenduRoy153 <126988308+ShirsenduRoy153@users.noreply.github.com>
2024-04-18 01:26:50 +0530

    first

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async(req, res, next) => {
-    res.render('abc.html', { title: 'Home' });
     return res.status(200).json({
         title: "Express Testing",
         message: "The app is working properly!",
