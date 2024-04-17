348cf8c266876e69d5ede8bff0f8ab758536658d
ShirsenduRoy153 <126988308+ShirsenduRoy153@users.noreply.github.com>
2024-04-18 01:55:46 +0530

    yrisiu

--- routes/home.js
+++ routes/home.js
@@ -18,7 +18,7 @@ app.get("/", async(req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
