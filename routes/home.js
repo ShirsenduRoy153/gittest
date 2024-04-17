4d49e105a6d268cec39be1f953c18d435ff5ca00
ShirsenduRoy153 <126988308+ShirsenduRoy153@users.noreply.github.com>
2024-04-18 02:18:40 +0530

    hello

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", async(req, res, next) => {
     return res.status(200).json({
         title: "Express Testing",
-        message: "DEKH NORMAL JSON TO DEKHATE PARCHE ~ BY SHIRSENDU EAI BAR HBS FILE DEKHATE HOBE",
+        message: "The app is working properly!",
     });
 });
 
