73a7c220f68b34b7e87bf047f2bdceb4515a95f6
ShirsenduRoy153 <126988308+ShirsenduRoy153@users.noreply.github.com>
2024-04-18 02:17:37 +0530

    hello

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const app = express()
-const path = require('path')
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 router.get("/", async(req, res, next) => {
-    // return res.status(200).json({
-    //     title: "Express Testing",
-    //     message: "The app is working properly!",
-    // });
-    return res.render('index', { title: 'Home' });
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "DEKH NORMAL JSON TO DEKHATE PARCHE ~ BY SHIRSENDU EAI BAR HBS FILE DEKHATE HOBE",
+    });
 });
 
 module.exports = router;
