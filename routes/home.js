865c1f2b3b817244cd75e7790056733e3bdabbc6
ShirsenduRoy153 <126988308+ShirsenduRoy153@users.noreply.github.com>
2024-04-18 02:11:31 +0530

    yrisisasassa

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 router.get("/", async(req, res, next) => {
-    return res.status(200).json({
-        title: "Express Testing",
-        message: "The app is working properly!",
-    });
+    // return res.status(200).json({
+    //     title: "Express Testing",
+    //     message: "The app is working properly!",
+    // });
+    return res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
